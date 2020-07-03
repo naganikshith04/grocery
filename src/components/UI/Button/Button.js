eb3fb48949c024e0d4b9848a601bfd2a63d37d4c
naganikshith04 <naganikshith02@gmail.com>
2020-07-03 18:07:28 +0530

    firebaseUpdated

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 const Bon = ( props ) => {
     const useStyles = makeStyles({
         root: {
-          // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
           backgroundImage:'-webkit-gradient(linear,left top,right top,from(#4a77f4),color-stop(51%,#00bade),to(#4a77f4)), -webkit-linear-gradient(left,#4a77f4,#00bade 51%,#4a77f4),linear-gradient(90deg,#4a77f4 0,#00bade 51%,#4a77f4)',
           borderRadius: 3,
           border: 0,
