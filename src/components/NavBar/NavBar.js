4480a3dba5bbb6a98644acbbf781ea71950fa75d
naganikshith04 <naganikshith02@gmail.com>
2020-07-04 12:16:05 +0530

    firebaseSceret

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,21 +6,6 @@ import {Navbar,Nav} from 'react-bootstrap'
 
 function NavBar() {
     return (
-      //   <Navbar style={{
-            
-      //   }} bg="primary" variant="dark">
-      //   <Navbar.Brand href="home">Groceries</Navbar.Brand>
-      //   <Nav className="mr-auto">
-      //     <Nav.Link href="home">Home</Nav.Link>
-      //     <Nav.Link href="features">Features</Nav.Link>
-      //     <Nav.Link href="pricing">Pricing</Nav.Link>
-      //   </Nav>
-      //   {/* <Form inline>
-      //     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      //     <Button variant="outline-light">Search</Button>
-      //   </Form> */}
-      // </Navbar>
-    
       <Navbar fixed="top"  style={{
         color:'white',
       }} bg="primary" expand="lg">
